ab1fe4f2d2aefe61aefd4f61f7eeff5bb369ceaf
ju-s11 <julia.g.s.jso@gmail.com>
2025-07-02 20:27:07 -0300

    lru funcional

--- simulador_memoria.c
+++ simulador_memoria.c
@@ -1,10 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+//estrutura para armazenar informações de uma página
+typedef struct {
+    int R; //flag de página referenciada
+    int M; //flag de página modificada
+    time_t ultimo_acesso; // tempo do último acesso (para LRU)
+} Pagina;
+
+void lru(Pagina *memoria, int numero_paginas, int tamanho_pagina, unsigned int addr, char rw, int *page_fault, int *pagina_suja) {
+    static int paginas_carregadas = 0; // quantidade de quadros ocupados
+    static unsigned page_size = 0;
+    static unsigned s = 0;
+
+    if (page_size == 0) {
+        page_size = tamanho_pagina * 1024;
+        unsigned tmp = page_size;
+        while (tmp > 1) {
+            tmp = tmp >> 1;
+            s++;
+        }
+    }
+
+    //determinando o número da página associada ao endereço
+    unsigned int pagina_atual = addr >> s;
+
+    //verificando se a página já está carregada na memória
+    for (int i = 0; i < paginas_carregadas; i++) {
+        if ((memoria[i].R == pagina_atual)) { //simulando presença da página usando R como "número"
+            memoria[i].ultimo_acesso = time(NULL);
+            memoria[i].R = pagina_atual;
+            if (rw == 'W') {
+                memoria[i].M = 1;
+            }
+            return; //página já está na memória
+        }
+    }
+
+    //caso a página não esteja na memória, incrementa o contador de page faults
+    (*page_fault)++;
+
+    //se ainda houver espaço na memória, apenas adiciona a nova página
+    if (paginas_carregadas < numero_paginas) {
+        memoria[paginas_carregadas].R = pagina_atual;
+        memoria[paginas_carregadas].M = (rw == 'W') ? 1 : 0;
+        memoria[paginas_carregadas].ultimo_acesso = time(NULL);
+        paginas_carregadas++;
+        return;
+    }
+
+    //se não houver espaço, substituir a página LRU (menor tempo), ou seja, o último acesso mais antigo
+    int lru_index = 0;
+    time_t mais_antigo = memoria[0].ultimo_acesso;
+
+    for (int i = 1; i < numero_paginas; i++) {
+        if (memoria[i].ultimo_acesso < mais_antigo) {
+            mais_antigo = memoria[i].ultimo_acesso;
+            lru_index = i;
+        }
+    }
+
+    //verificando se a página que vai sair foi modificada, ou seja, se é uma página suja
+    if (memoria[lru_index].M == 1) {
+        (*pagina_suja)++;
+    }
+
+    //substitui pela nova página
+    memoria[lru_index].R = pagina_atual;
+    memoria[lru_index].M = (rw == 'W') ? 1 : 0;
+    memoria[lru_index].ultimo_acesso = time(NULL);
+}
+
+void segundo_chance() {
+}
+
+void algoritmo_clock() { //função clock não pode ter o nome clock(), pois é uma função padrão da biblioteca time.h
+}
+
+void otimo() {
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 5) {
-        fprintf(stderr, "Uso: %s <algoritmo> <arquivo.log> <tamanho de cada pagina em KB> <tamanho da memoria em MB>\n", argv[0]);
+        fprintf(stderr, "Uso: %s <algoritmo> <arquivo do tipo .log> <tamanho de cada pagina em KB> <tamanho da memoria em MB>\n", argv[0]);
         return 1;
     }
 
@@ -42,13 +122,41 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    //leitura do arquivo linha por linha
     unsigned int addr;
     char rw;
+    int pagina_suja = 0;
+    int page_fault = 0;
+    int numero_paginas = tamanho_memoria * 1024 / tamanho_pagina; // número de páginas na memória
+    //printf("Número de páginas: %d\n", numero_paginas);
+
+    Pagina memoria[numero_paginas];
+
+    //leitura do arquivo linha por linha
     while (fscanf(arquivo, "%x %c", &addr, &rw) == 2) {
         printf("Endereço: 0x%08x, Tipo: %c\n", addr, rw);
-        //continuar simulação AQUI
+        if (rw != 'R' && rw != 'W') {
+            fprintf(stderr, "Erro: tipo de operação inválido '%c'. Use 'R' ou 'W'\n", rw);
+            fclose(arquivo);
+            return 1;
+        }
+
+        if (strcmp(algoritmo, "LRU") == 0) {
+            lru(memoria, numero_paginas, tamanho_pagina,addr, rw, &page_fault, &pagina_suja);
+        } else if (strcmp(algoritmo, "2nd") == 0) {
+            printf("Algoritmo não foi implementado 2nd ainda!\n");
+            //segundo_chance();
+        } else if (strcmp(algoritmo, "clock") == 0) {
+            printf("Algoritmo não foi implementado clock ainda!\n");
+            //clock();
+        } else if (strcmp(algoritmo, "otimo") == 0) {
+            printf("Algoritmo não foi implementado ótimo ainda!\n");
+            //otimo();
+        }
     }
+
+    //imprimindo resultados
+    printf("Número de page faults: %d\n", page_fault);
+    printf("Número de páginas sujas: %d\n", pagina_suja);  
 
     fclose(arquivo);
     return 0;
