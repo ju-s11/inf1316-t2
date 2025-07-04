ca904993131ab1b4a8b5c79773cb39f265572688
Luiza Rég <128841255+LuRegnier@users.noreply.github.com>
2025-07-04 13:27:27 -0300

    Update simulador_memoria.c
     algoritmo ótmo ta ai já e a main ta chamando quando for lido do arquivo.

--- simulador_memoria.c
+++ simulador_memoria.c
@@ -79,8 +79,93 @@ void segundo_chance() {
 void algoritmo_clock() { //função clock não pode ter o nome clock(), pois é uma função padrão da biblioteca time.h
 }
 
-void otimo() {
+typedef struct {
+    unsigned int pagina;
+    int M; // Página modificada
+} Quadro;
+
+typedef struct {
+    unsigned int addr;
+    char rw;
+} Acesso;
+
+void otimo(Pagina *memoria, int numero_paginas, int tamanho_pagina, Acesso *acessos, int total_acessos, int *page_fault, int *pagina_suja) {
+    static int paginas_carregadas = 0;
+    static unsigned page_size = 0;
+    static unsigned s = 0;
+    static Quadro *quadros = NULL;
+    static int pos = 0;
+
+    if (quadros == NULL) {
+        quadros = (Quadro *)malloc(sizeof(Quadro) * numero_paginas);
+    }
+
+    if (page_size == 0) {
+        page_size = tamanho_pagina * 1024;
+        unsigned tmp = page_size;
+        while (tmp > 1) {
+            tmp = tmp >> 1;
+            s++;
+        }
+    }
+
+    while (pos < total_acessos) {
+        unsigned int pagina_atual = acessos[pos].addr >> s;
+        char rw = acessos[pos].rw;
+
+        // Verifica se a página já está na memória
+        int encontrada = 0;
+        for (int i = 0; i < paginas_carregadas; i++) {
+            if (quadros[i].pagina == pagina_atual) {
+                if (rw == 'W') quadros[i].M = 1;
+                encontrada = 1;
+                break;
+            }
+        }
+
+        if (!encontrada) {
+            (*page_fault)++;
+
+            // Se ainda há espaço na memória, insere diretamente
+            if (paginas_carregadas < numero_paginas) {
+                quadros[paginas_carregadas].pagina = pagina_atual;
+                quadros[paginas_carregadas].M = (rw == 'W') ? 1 : 0;
+                paginas_carregadas++;
+            } else {
+                // Substituição ótima
+                int mais_distante = -1, indice_substituir = -1;
+
+                for (int i = 0; i < numero_paginas; i++) {
+                    int j;
+                    for (j = pos + 1; j < total_acessos; j++) {
+                        unsigned int prox_pag = acessos[j].addr >> s;
+                        if (prox_pag == quadros[i].pagina) break;
+                    }
+
+                    if (j == total_acessos) {
+                        // Página nunca mais será usada
+                        indice_substituir = i;
+                        break;
+                    } else if (j > mais_distante) {
+                        mais_distante = j;
+                        indice_substituir = i;
+                    }
+                }
+
+                if (quadros[indice_substituir].M == 1)
+                    (*pagina_suja)++;
+
+                quadros[indice_substituir].pagina = pagina_atual;
+                quadros[indice_substituir].M = (rw == 'W') ? 1 : 0;
+            }
+        }
+
+        pos++;
+    }
+
+    free(quadros);
 }
+
 
 int main(int argc, char *argv[]) {
     if (argc != 5) {
@@ -126,10 +211,36 @@ int main(int argc, char *argv[]) {
     char rw;
     int pagina_suja = 0;
     int page_fault = 0;
-    int numero_paginas = tamanho_memoria * 1024 / tamanho_pagina; // número de páginas na memória
+    int numero_paginas = tamanho_memoria * 1024 / tamanho_pagina; // n= de páginas na memória
     //printf("Número de páginas: %d\n", numero_paginas);
+    // ^^^testar^^
 
     Pagina memoria[numero_paginas];
+
+    // Se algoritmo for otimo tem que ler todos os acessos antes
+Acesso *acessos = NULL;
+int total_acessos = 0;
+
+if (strcmp(algoritmo, "otimo") == 0) {
+    // Primeira leitura
+    unsigned int a;
+    char r;
+    while (fscanf(arquivo, "%x %c", &a, &r) == 2)
+        total_acessos++;
+
+    acessos = malloc(sizeof(Acesso) * total_acessos);
+    rewind(arquivo); //func pra voltar tudo no arquivo maneiro né
+
+    for (int i = 0; i < total_acessos; i++)
+        fscanf(arquivo, "%x %c", &acessos[i].addr, &acessos[i].rw);
+
+    // Executar algoritmo ótimo
+    Pagina memoria_otima[numero_paginas];
+    otimo(memoria_otima, numero_paginas, tamanho_pagina, acessos, total_acessos, &page_fault, &pagina_suja);
+    free(acessos);
+    fclose(arquivo);
+}
+
 
     //leitura do arquivo linha por linha
     while (fscanf(arquivo, "%x %c", &addr, &rw) == 2) {
